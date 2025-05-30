d40cee4172883d33ff565b219fd5f36d44bddf51
cemile.blks <cemilebalkas@gmail.com>
2025-05-30 15:57:40 +0100

    improved script: sort and compress the output file

--- format_for_dss.sh
+++ format_for_dss.sh
@@ -4,14 +4,16 @@
 # Script: format_for_dss.sh
 # Purpose: Convert Oxford Nanopore bedMethyl files to DSS input format
 # Usage: ./format_for_dss.sh input_file.bed
-# Output: A new file named dss_data_<input_file>.bed with 4 columns:
+# Output: A new file named <input_file>.dssin.gz with 4 columns DSS expects:
 #         - chr (column 1)
 #         - pos (column 2, 0-based start position)
 #         - N (column 10 = Nvalid_cov, total reads at site)
 #         - X (column 12 = Nmod, number of methylated reads)
 #
-# Note:
+# Notes:
 #   - DSS expects chr, pos, N, X
+#   - Script sorts by chromosome and position
+#   - Script compresses output with gzip
 # -----------------------------------------------------------------------------
 
 # Check if user provided a file
@@ -22,22 +24,17 @@ if [ -z "$1" ]; then
 fi
 
 INPUT_FILE=$1
-# Extract base name (remove path) amd change file extenstion to dssin to signify file input for dss
+# Extract base name (remove path)
 BASENAME=$(basename "$INPUT_FILE")
-OUTFILE="dss_data_${BASENAME%.*}.dssin"
+BASENAME_NOEXT="${BASENAME%.*}" # reomve extension
+OUTFILE="${BASENAME_NOEXT}.dssin.gz"
 
 echo "Processing file: $INPUT_FILE"
 echo "Saving output to: $OUTFILE"
 
-# Extract needed columns: chr, pos, Nvalid_cov (col 10), Nmod (col 12)
-awk 'BEGIN{OFS="\t"} {print $1, $2, $10, $12}' "$INPUT_FILE" > "$OUTFILE"
+# Extract needed columns: chr, pos, Nvalid_cov (col 10), Nmod (col 12). Sort and compress.
+awk 'BEGIN{OFS="\t"} {print $1, $2, $10, $12}' "$INPUT_FILE" | sort -k1,1 -k2,2n | gzip > "$OUTFILE"
 
 # Confirm success
 echo "Conversion complete. Output file: $(realpath "$OUTFILE")"
 ls -lh "$OUTFILE"
-
-# UPDATES TO MAKE:
-# CHECK IF IT IS OXFORD NANAOPORE BEDMETHYL FILE (CHECK NUMBER OF COLOUMNS ARE 18 AND AND IF POS, N AND X ARE NUMBERS) 
-# SORT BY CHROMOSOME AND POSITION BEFORE SAVING TO OUTPUT FILE 
-# CHANGE THE OUTPUT FILE TO .DSS INSTEAD OF BED FILES BECAUSE THEY ARE NOT BED FILES
-# GZIP THE FILE OUTPUT BECAUSE R CAN READ GZIPPED FILES AND TO SAVE SPACE
