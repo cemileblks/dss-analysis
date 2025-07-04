9e354668f2054d5d3c801b6363b7f4c00028551b
cemileblks <cemilebalkas@gmail.com>
2025-07-04 15:16:14 +0100

    cleared up code logic and improved outputs

--- data/annotation_data/defining_genomic_regions.sh
+++ data/annotation_data/defining_genomic_regions.sh
@@ -1,88 +1,116 @@
 #!/usr/bin/bash
 
-wget ftp://ftp.ensembl.org/pub/release-114/gtf/homo_sapiens/Homo_sapiens.GRCh38.114.gtf.gz -P ./
+# This script defines exons, introns, TSS, UTRs, downstream, and intergenic regions from Ensembl GTF
 
-# The input GTF file is compressed in .gz format
-GTF_FILE="Homo_sapiens.GRCh38.114.gtf.gz"
-EXON_MERGED_FILE="Homo_sapiens.GRCh38.114_exons_merged.bed"
-INTRON_FILE="Homo_sapiens.GRCh38.114_introns.bed"
-TSS_FILE="Homo_sapiens.GRCh38.114_tss_merged.bed"
-THREE_PRIME_UTR_MERGED_FILE="Homo_sapiens.GRCh38.114_3utrs.bed"
-FIVE_PRIME_UTR_MERGED_FILE="Homo_sapiens.GRCh38.114_5utrs.bed"
-DOWNSTREAM_FILE="Homo_sapiens.GRCh38.114_downstream_1kb_merged.bed"
-# everything that does not include the rest of the files above 
+# Parameters
+RELEASE="114"
+GENOME_NAME="hg38"
+GTF_FILE="Homo_sapiens.GRCh38.${RELEASE}.gtf.gz"
+FA_FILE="Homo_sapiens.GRCh38.dna.primary_assembly.fa"
+FAI_FILE="${FA_FILE}.fai"
+GENOME_FILE="genome_file.txt"
+GENOME_FILE_CANONICAL="genome_file_canonical.txt"
+GENOME_FILE_AUTOSOMAL="genome_file_autosomal.txt"
+GTF_PROTEIN_CODING="${GENOME_NAME}_protein_coding_only.gtf.gz"
+
+EXONS="${GENOME_NAME}_exons.bed"
+INTRONS="${GENOME_NAME}_introns.bed"
+TSS="${GENOME_NAME}_tss.bed"
+UTR5="${GENOME_NAME}_5utrs.bed"
+UTR3="${GENOME_NAME}_3utrs.bed"
+DOWNSTREAM="${GENOME_NAME}_downstream_1kb.bed"
+# everything that does not include the rest of the files above
 # (note: the exons or other features of IncRNA etc are also considered intergenic in this case)
-INTERGENIC_FILE="Homo_sapiens.GRCh38.114_intergenic_regions.bed" 
+INTERGENIC="${GENOME_NAME}_intergenic.bed"
+GENIC_MERGED="genic_regions_merged.bed"
 
-# Count the different types of features in the GTF file
-echo "Counting the different types of features in the input file:"
-zcat "$GTF_FILE" | grep -v "^#" | cut -f3 | sort | uniq -c | sort -k1rn
+# # Download data
+# echo "Downloading Ensembl GTF (release $RELEASE)..."
+# wget -nc ftp://ftp.ensembl.org/pub/release-${RELEASE}/gtf/homo_sapiens/${GTF_FILE} -P ./
 
-# Extract exons and merge overlapping/adjacent regions
+# echo "Downloading reference genome..."
+# wget -nc "ftp://ftp.ensembl.org/pub/release-${RELEASE}/fasta/homo_sapiens/dna/${FA_FILE}.gz"
+# gunzip -c "${FA_FILE}.gz" > "$FA_FILE"
+
+# # Index FASTA and extract genome sizes
+# samtools faidx "$FA_FILE"
+# cut -f1,2 "$FAI_FILE" > "$GENOME_FILE"
+# grep -E '^([1-9]|1[0-9]|2[0-2]|X|Y|MT)[[:space:]]' "$GENOME_FILE" | sort -k1,1 > "$GENOME_FILE_CANONICAL"
+# grep -E '^([1-9]|1[0-9]|2[0-2])[[:space:]]' "$GENOME_FILE" | sort -k1,1 > "$GENOME_FILE_AUTOSOMAL"
+# echo "----------------------------------------"
+
+# # Count the different types of features in the GTF file
+# echo "Counting the different types of features in the input file:"
+# zcat "$GTF_FILE" | grep -v "^#" | cut -f3 | sort | uniq -c | sort -k1rn
+# echo "----------------------------------------"
+
+# Define genomic features
+echo "Filtering for protein-coding features..."
+zcat "$GTF_FILE" |
+    grep "protein_coding" | gzip >"$GTF_PROTEIN_CODING"
+
+echo "Extracting all protein-coding gene coordinates (gene_id only)..."
+zcat "$GTF_PROTEIN_CODING" |
+    grep -E '^([1-9]|1[0-9]|2[0-2])[[:space:]]' |
+    awk 'BEGIN{OFS="\t"}
+     $3 == "gene" {
+         match($0, /gene_id "([^"]+)"/, gid);
+         if (gid[1] != "") {
+             print $1, $4-1, $5, gid[1];
+         }
+     }' |
+    sortBed >"${GENOME_NAME}_protein_coding_genes.bed"
+
 echo "Extracting and merging exons:"
-zcat "$GTF_FILE" | \
-grep "protein_coding" | \
-awk 'BEGIN{OFS="\t";} $3=="exon" {print $1, $4-1, $5}' | \
-sortBed | \
-mergeBed -i - > "$EXON_MERGED_FILE"
+zcat "$GTF_PROTEIN_CODING" |
+    grep -E '^([1-9]|1[0-9]|2[0-2])[[:space:]]' |
+    awk 'BEGIN{OFS="\t";} $3=="exon" {print $1, $4-1, $5}' |
+    sortBed |
+    mergeBed -i - >"$EXONS"
 
-# # Extract gene regions and subtract exon regions to get introns
-echo "Extracting introns by subtracting exons from gene regions:"
-zcat "$GTF_FILE" | \
-grep "protein_coding" | \
-awk 'BEGIN{OFS="\t";} $3=="gene" {print $1, $4-1, $5}' | \
-sortBed | \
-subtractBed -a stdin -b "$EXON_MERGED_FILE" > "$INTRON_FILE"
+# Extract gene regions and subtract exon regions to get introns
+echo "Extracting introns... (Subtracting exons from gene regions)"
+zcat "$GTF_PROTEIN_CODING" |
+    grep -E '^([1-9]|1[0-9]|2[0-2])[[:space:]]' |
+    awk 'BEGIN{OFS="\t";} $3=="gene" {print $1, $4-1, $5}' |
+    sortBed |
+    subtractBed -a stdin -b "$EXONS" >"$INTRONS"
 
-# # Let's intersect exons and introns (this should produce no output as exons and introns shouldn't overlap)
+# Let's intersect exons and introns (this should produce no output as exons and introns shouldn't overlap)
 echo "Intersecting exons and introns to check (should produce no output):"
-intersectBed -a "$EXON_MERGED_FILE" -b "$INTRON_FILE"
+intersectBed -a "$EXONS" -b "$INTRONS"
 
-# echo "getting chromosome lengths file"
-wget ftp://ftp.ensembl.org/pub/release-114/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz
-gunzip Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz
-samtools faidx Homo_sapiens.GRCh38.dna.primary_assembly.fa
-cut -f1,2 Homo_sapiens.GRCh38.dna.primary_assembly.fa.fai > genome_file.txt
-grep -E '^([1-9]|1[0-9]|2[0-2]|X|Y|MT)[[:space:]]' genome_file.txt | sort -k1,1 > genome_file_canonical.txt
-GENOME_FILE="genome_file_canonical.txt"
-
-echo "Extracting TSS regions around from gene start pos (±200bp):"
-zcat "$GTF_FILE" | \
-grep -E '^([1-9]|1[0-9]|2[0-2])[[:space:]]' | \
-grep "protein_coding" | \
-awk 'BEGIN{OFS="\t"}
+echo "Extracting promoters (±200bp TSS)..."
+zcat "$GTF_PROTEIN_CODING" |
+    grep -E '^([1-9]|1[0-9]|2[0-2])[[:space:]]' |
+    awk 'BEGIN{OFS="\t"}
      $3=="gene" {
          chrom=$1;
          start=($7=="+" ? $4-201 : $5-200);
          end=($7=="+" ? $4+199 : $5+200);
          if (start < 0) start = 0;
-         print chrom, start, end}' | \
-bedtools sort -i - -g genome_file_canonical.txt | \
-mergeBed -i - > "$TSS_FILE"
+         print chrom, start, end}' |
+    bedtools sort -i - -g "$GENOME_FILE_CANONICAL" |
+    mergeBed -i - >"$TSS"
 
-# Extract 5utrs and 3utrs
-echo "Extracting and merging 5 prime utrs:"
-zcat "$GTF_FILE" | \
-grep "protein_coding" | \
-grep -E '^([1-9]|1[0-9]|2[0-2])[[:space:]]' | \
-awk 'BEGIN{OFS="\t";} $3=="five_prime_utr" {print $1, $4-1, $5}' | \
-sortBed | \
-mergeBed -i - > "$FIVE_PRIME_UTR_MERGED_FILE"
+echo "Extracting 5' UTRs..."
+zcat "$GTF_PROTEIN_CODING" |
+    grep -E '^([1-9]|1[0-9]|2[0-2])[[:space:]]' |
+    awk 'BEGIN{OFS="\t";} $3=="five_prime_utr" {print $1, $4-1, $5}' |
+    sortBed |
+    mergeBed -i - >"$UTR5"
 
-echo "Extracting and merging 3 prime utrs:"
-zcat "$GTF_FILE" | \
-grep "protein_coding" | \
-grep -E '^([1-9]|1[0-9]|2[0-2])[[:space:]]' | \
-awk 'BEGIN{OFS="\t";} $3=="three_prime_utr" {print $1, $4-1, $5}' | \
-sortBed | \
-mergeBed -i - > "$THREE_PRIME_UTR_MERGED_FILE"
+echo "Extracting 3' UTRs..."
+zcat "$GTF_PROTEIN_CODING" |
+    grep -E '^([1-9]|1[0-9]|2[0-2])[[:space:]]' |
+    awk 'BEGIN{OFS="\t";} $3=="three_prime_utr" {print $1, $4-1, $5}' |
+    sortBed |
+    mergeBed -i - >"$UTR3"
 
-# Extract Immediate downstream
-echo "Extracting immediate downstream regions of protein coding genes"
-zcat "$GTF_FILE" | \
-grep -E '^([1-9]|1[0-9]|2[0-2])[[:space:]]' | \
-grep "protein_coding" | \
-awk 'BEGIN{OFS="\t"}
+echo "Extracting immediate downstream regions..."
+zcat "$GTF_PROTEIN_CODING" |
+    grep -E '^([1-9]|1[0-9]|2[0-2])[[:space:]]' |
+    awk 'BEGIN{OFS="\t"}
      $3=="gene" {
          chrom=$1;
          strand=$7;
@@ -94,27 +122,38 @@ awk 'BEGIN{OFS="\t"}
              end = $4 - 1;
              if (start < 0) start = 0;
          }
-         print chrom, start, end;}' | \
-bedtools sort -i - -g genome_file_canonical.txt | \
-mergeBed -i - > "$DOWNSTREAM_FILE"
+         print chrom, start, end;}' |
+    bedtools sort -i - -g "$GENOME_FILE_CANONICAL" |
+    mergeBed -i - >"$DOWNSTREAM"
 
-echo "Combining genic regions (exons, introns, UTRs, promoters, downstream)"
-cat "$EXON_MERGED_FILE" \
-    "$INTRON_FILE" \
-    "$TSS_FILE" \
-    "$THREE_PRIME_UTR_MERGED_FILE" \
-    "$FIVE_PRIME_UTR_MERGED_FILE" \
-    "$DOWNSTREAM_FILE" | \
-sort -k1,1 -k2,2n | \
-mergeBed -i - > genic_regions_merged.bed
+# Merge all genic regions
+echo "Merging all genic regions..."
+cat "$EXONS" "$INTRONS" "$TSS" "$UTR5" "$UTR3" "$DOWNSTREAM" |
+    sort -k1,1 -k2,2n |
+    mergeBed -i - >"$GENIC_MERGED"
 
-# Extract intergenic regions (complement of protein coding genes)
-echo "Extracting intergenic regions (complement of genic features):"
+# Define intergenic regions
+echo "Extracting intergenic regions..."
+bedtools complement -i "$GENIC_MERGED" -g "$GENOME_FILE_AUTOSOMAL" >"$INTERGENIC"
 
-grep -E '^([1-9]|1[0-9]|2[0-2])[[:space:]]' genic_regions_merged.bed | \
-bedtools complement -i - -g "$GENOME_FILE" > "$INTERGENIC_FILE"
-
-# Converst evertything to ucsc style chr1 chr2 etc for compatability with dss output
-for f in *_merged.bed *_regions.bed *utrs.bed *introns.bed; do
-    awk 'BEGIN{OFS="\t"} {$1 = ($1 == "MT" ? "chrM" : "chr" $1); print}' "$f" > tmp && mv tmp "$f"
+# Convert chromosome names to UCSC style
+echo "Converting chromosome names to UCSC style (only if not already prefixed)..."
+for f in hg38_*.bed; do
+    awk 'BEGIN{OFS="\t"}
+       {
+         if ($1 ~ /^chr/) {
+           print
+         } else {
+           $1 = ($1 == "MT" ? "chrM" : "chr"$1);
+           print
+         }
+       }' "$f" >tmp && mv tmp "$f"
 done
+
+# Cleanup large files
+echo "Cleaning up temporary files..."
+rm -f "$FA_FILE" "$FAI_FILE" "$GTF_PROTEIN_CODING" "$GENIC_MERGED"
+
+echo "----------------------------------------"
+echo "Done. Output files:"
+ls -1 hg38_*.bed
